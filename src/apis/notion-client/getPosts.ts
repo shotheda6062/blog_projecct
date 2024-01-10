0b28dfdaa72bc836b9570045690033be3eb02a06
shotheda6062 <shothead6062@gmail.com>
2024-01-10 20:10:00 +0800

    Update getPosts.ts

--- src/apis/notion-client/getPosts.ts
+++ src/apis/notion-client/getPosts.ts
@@ -13,6 +13,9 @@ import { TPosts } from "src/types"
 // TODO: react query를 사용해서 처음 불러온 뒤로는 해당데이터만 사용하도록 수정
 export const getPosts = async () => {
   let id = CONFIG.notionConfig.pageId as string
+
+  console.log('-----------------------------' + id);
+  
   const api = new NotionAPI()
 
   const response = await api.getPage(id)
