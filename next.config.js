0fb1cf1e626640b6dd398b225e3a8354e465f258
shotheda6062 <shothead6062@gmail.com>
2024-01-26 17:38:38 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/blog_projecct',
+
+}
+
 module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
