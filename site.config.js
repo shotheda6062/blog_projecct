0cd932f7ffb74a458e733b9d48540f834a473698
shotheda6062 <shothead6062@gmail.com>
2024-01-29 11:29:16 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "clarke",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/blog_projecct/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "shothead6062@gmail.com",
